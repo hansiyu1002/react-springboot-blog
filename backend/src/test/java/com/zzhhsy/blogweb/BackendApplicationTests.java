c0f7232b48a106365e020848924152bc719c58d2
hansiyu1002 <hansiyu1002@outlook.com>
2022-09-09 04:51:24 +0800

    Initial commit of backend

---
+++ backend/src/test/java/com/zzhhsy/blogweb/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzhhsy.blogweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
