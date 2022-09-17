97f54852bb807b27a8df334be6835a6e570e2c7d
hansiyu1002 <hansiyu1002@outlook.com>
2022-09-17 20:21:54 +0800

    Add LogAspect for controller methods

---
+++ backend/src/main/java/com/zzhhsy/blogweb/log/LogAspect.java
@@ -0,0 +1,38 @@
+package com.zzhhsy.blogweb.log;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Slf4j
+public class LogAspect {
+    @Pointcut("execution(* com.zzhhsy.blogweb.controller.*.*(..))")
+    public void pointcut() {}
+
+    @Around("pointcut()")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+        Long start = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        Long end = System.currentTimeMillis();
+
+        log.info("--------------------LogAspect--------------------");
+        String className = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+        log.info("method: {}", className + "." + methodName);
+
+        Object[] args = joinPoint.getArgs();
+        ObjectMapper mapper = new ObjectMapper();
+        String argsJson = mapper.writeValueAsString(args);
+        log.info("params: {}", argsJson);
+
+        log.info("execution time: {} ms", end - start);
+        log.info("--------------------LogAspect--------------------");
+        return result;
+    }
+}
