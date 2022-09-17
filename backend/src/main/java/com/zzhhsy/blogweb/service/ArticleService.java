7e5997dc34db6a23b260cffd3ed6995a22da1c22
hansiyu1002 <hansiyu1002@outlook.com>
2022-09-17 20:53:07 +0800

    @Transactional

--- backend/src/main/java/com/zzhhsy/blogweb/service/ArticleService.java
+++ backend/src/main/java/com/zzhhsy/blogweb/service/ArticleService.java
@@ -2,7 +2,9 @@ package com.zzhhsy.blogweb.service;
 
 import com.zzhhsy.blogweb.vo.ArticleParam;
 import com.zzhhsy.blogweb.vo.Result;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface ArticleService {
     Result getArticleById(Long id);
     Result getMyArticles();
