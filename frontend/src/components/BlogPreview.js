09e98e125882b84ec145c1065c30a7282f1da476
hansiyu1002 <hansiyu1002@outlook.com>
2022-09-19 19:38:08 +0800

    Display view count at BlogPreview

--- frontend/src/components/BlogPreview.js
+++ frontend/src/components/BlogPreview.js
@@ -2,9 +2,10 @@ import React from 'react';
 import {Button, ButtonGroup, Card} from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useDeleteBlogMutation } from "../services/apiSlice";
+import {BsEye} from "react-icons/bs";
 
 function BlogPreview({ blog }) {
-    const { title, content, id } = blog;
+    const { title, content, viewCount, id } = blog;
     const [deleteBlog] = useDeleteBlogMutation();
     function handleDelete(e) {
         e.preventDefault();
@@ -14,7 +15,15 @@ function BlogPreview({ blog }) {
     return (
         <Card style={{width: "18rem"}}>
             <Card.Body>
-                <Card.Title>{title}</Card.Title>
+                <Card.Title>
+                    {title}
+                    <div className="text-muted float-right" style={{font: "initial"}}>
+                        <BsEye/>
+                        <span>
+                            {viewCount}
+                        </span>
+                    </div>
+                </Card.Title>
                 <Card.Text dangerouslySetInnerHTML={{ __html: content?.substring(0, 100) + '...' }} />
                 <ButtonGroup>
                     <LinkContainer to={`/my_blog/${id}`}>
